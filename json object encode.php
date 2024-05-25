8fa350bc6426eb18bae239cca6dc63e505f9d8dd
rakib <rakibweb68@gmail.com>
2024-05-25 14:22:16 +0600

    added

--- json object encode.php
+++
@@ -1,6 +0,0 @@
-<?php
-//Associative array to json object encode
-
-$person=["name"=>"Rakibul Hassan  Babo", "age"=>24];
-$jSON=json_encode($person);
-echo $jSON;
--- part1.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//Associative Array to JSON Object Encode
-
-$person=["name"=>"OSTAD","age"=>34];
-
-$JSON=json_encode($person);
-
-echo $JSON;
